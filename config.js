9ad9725f0cc73f5e11f1500098bed3166b248ed1
Austin <48300512+SerDubstepCat@users.noreply.github.com>
2021-10-18 20:35:43 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const PORT = process.env.PORT || 5000;
 const PASSWORD = process.env.GIMPTRACKER_PASSWORD || "fletchgangosrs";
-const CONFIG_BACKEND_URL = 'https://fletchgang.herokuapp.com/'
 
 module.exports = 
 {
